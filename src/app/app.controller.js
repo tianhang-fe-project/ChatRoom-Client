0af89e45f9e8e7ebd21ccd544a81f4d3ab6e34db
Zhang Tianhang <tianhang1991@126.com>
2017-09-02 21:41:20 +0800

    Send msg

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,7 +1,8 @@
 import logo from '../public/img/logo.png'
 import avatar from '../public/img/avatar/avatar1.jpg'
-import SockJS from 'sockjs-client'
-import chatClient from './chatclient.js'
+import io from 'socket.io-client';
+// import SockJS from 'sockjs-client'
+// import chatClient from './chatclient.js'
 
 class AppCtrl {
   constructor() {
@@ -9,49 +10,129 @@ class AppCtrl {
     console.log(logo);
     this.logo = logo;
     this.avatar = avatar;
-    console.log("---socket--");
-   
-    this.initClient();
-    
+    this.mode = 'room';
+    this.userInfo = {};
+    this.msgList = [];
+    //this.initClient();
+    var socket = io('http://localhost:3000?roomid=k12');
+    this.socket = socket;
+    console.log("socket:", socket)
+    socket.on('connect', function() {
+      console.log("connect ...");
+      console.log("socket id:", socket.id)
+      socket.emit('join', {
+        username: 'tianhang'
+      });
+    });
+
+    socket.on('event', function(data) {
+      console.log(data);
+    });
+
+    socket.on('disconnect', function() {
+      console.log("disconnect ...");
+    });
+
+    socket.on('message', (msg) => {
+      console.log('msg:', msg);
+      switch (msg.event) {
+        case 'join':
+          if (msg.data.username) {
+            console.log(msg.data.username + '加入了聊天室');
+            var data = {
+              text: msg.data.username + '加入了聊天室'
+            };
+            //showNotice(data);
+          }
+          break;
+        case 'broadcast_say':
+          // console.log("this:", this)
+          if (msg.data.username !== this.userInfo.username) {
+            console.log(msg.data.username + '说: ' + msg.data.text);
+            //showMessage(msg.data);
+            this.msgList.push(msg);
+          }
+          break;
+        case 'broadcast_quit':
+          if (msg.data.username) {
+            console.log(msg.data.username + '离开了聊天室');
+            var data = {
+              text: msg.data.username + '离开了聊天室'
+            };
+            //showNotice(data);
+          }
+          break;
+      }
+    })
+
   }
-  initClient(){
-    let options ={
-      url : "http://localhost:8888/daemon"
-    };
-    let client = new chatClient(options,this.onClose,this.onOpen,this.onMessage)
-    // console.log(sock); 
+
+  onSubmit(msg) {
+    this.sendRoomMsg(msg, 'k12');
   }
-  
-  
-  sendMsg(msg,socketId){
-    
+
+  sendMsg(text, socketId) {
+    this.socket.emit('msg', {
+      username: 'tianhang',
+      text: text,
+      id: socketId,
+      type: 'msg'
+    });
   }
-  
-  sendGroupMsg(msg,roomId){
-    
+
+  sendRoomMsg(text, roomId) {
+    this.socket.emit('roommsg', {
+      username: 'tianhang',
+      text: text,
+      id: roomId,
+      type: 'roommsg'
+    });
   }
-  
-  //callbak functions
-  onOpen(){
-    console.log('open');
-    console.log(this)
-    this.send('join ' + JSON.stringify({
-           tid: "123456"
-      }));
-    this.send('post ' +'hello world!'); 
+  sendGroupMsg(text, groupId) {
+
   }
+
+
+
+
+
+  /*
+    initClient(){
+      let options ={
+        url : "http://localhost:8888/daemon"
+      };
+      let client = new chatClient(options,this.onClose,this.onOpen,this.onMessage)
+      
+    }
   
-  onMessage(e){
-    console.log('msg');
-    console.log(e);
-    console.log('message:', e.data);
-  }
+    sendMsg(msg,socketId){
+      
+    }
   
-  onClose(){
-    console.log('close');
-  }
+    sendGroupMsg(msg,roomId){
+      
+    }
   
-  onMouseover(){
+    onOpen(){
+      console.log('open');
+      console.log(this)
+      this.send('join ' + JSON.stringify({
+             tid: "123456"
+        }));
+      this.send('post ' +'hello world!'); 
+    }
+  
+    onMessage(e){
+      console.log('msg');
+      console.log(e);
+      console.log('message:', e.data);
+    }
+  
+    onClose(){
+      console.log('close');
+    }*/
+
+  onMouseover() {
     console.log("over...");
     this.showMsgBox = true;
   }
